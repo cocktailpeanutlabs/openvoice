742091e90a7449e5a5e8bb0825c1d0c20aa59bdb
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-22 15:32:37 -0700

    update

--- install.js
+++ install.js
@@ -1,40 +1,62 @@
-module.exports = {
-  "cmds": {
-    "nvidia": "pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118",
-    "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6",
-    "default": "pip install torch torchvision torchaudio"
-  },
-  "requires": [{
-    "type": "conda",
-    "name": "ffmpeg",
-    "args": "-c conda-forge"
-  }],
-  "run": [{
-    "method": "shell.run",
-    "params": {
-      "venv": "env",
-      "message": "git clone https://huggingface.co/spaces/cocktailpeanut/OpenVoice app",
+module.exports = async (kernel) => {
+  let cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu"
+  if (kernel.platform === 'darwin') {
+    if (kernel.arch === "arm64") {
+      cmd = "uv pip install torch torchaudio torchvision"
+    } else {
+      cmd = "uv pip install torch==2.1.2 torchaudio==2.1.2"
     }
-  }, {
-    "method": "shell.run",
-    "params": {
-      "path": "app",
-      "venv": "env",
-      "message": [
-        "pip install -r requirements_locally.txt",
-        "{{(gpu === 'nvidia' ? self.cmds.nvidia : (gpu === 'amd' ? self.cmds.amd : self.cmds.default))}}"
-      ]
-    }
-  }, {
-    "method": "fs.download",
-    "params": {
-      "uri": "https://myshell-public-repo-host.s3.amazonaws.com/openvoice/checkpoints_1226.zip",
-      "path": "app/ckpt.zip"
-    }
-  }, {
-    "method": "notify",
-    "params": {
-      "html": "Click the 'start' tab to get started!"
-    }
-  }]
+  } else {
+    if (kernel.gpu === 'nvidia') {
+      if (kernel.gpu_model && / 50.+/.test(kernel.gpu_model)) {
+        cmd = "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+      } else {
+        cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 xformers --index-url https://download.pytorch.org/whl/cu124"
+      }
+    } else if (kernel.gpu === 'amd') {
+      cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/rocm6.2"
+    } 
+  //}
+mo//dule.exports = {
+  //"cmds": {
+  //  "nvidia": "pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118",
+  //  "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6",
+  //  "default": "pip install torch torchvision torchaudio"
+  //},
+  //"requires": [{
+  //  "type": "conda",
+  //  "name": "ffmpeg",
+  //  "args": "-c conda-forge"
+  //}],
+  return {
+    "run": [{
+      "method": "shell.run",
+      "params": {
+        "venv": "env",
+        "message": "git clone https://huggingface.co/spaces/cocktailpeanut/OpenVoice app",
+      }
+    }, {
+      "method": "shell.run",
+      "params": {
+        "path": "app",
+        "venv": "env",
+        "message": [
+          "pip install -r requirements_locally.txt",
+          cmd
+//          "{{(gpu === 'nvidia' ? self.cmds.nvidia : (gpu === 'amd' ? self.cmds.amd : self.cmds.default))}}"
+        ]
+      }
+    }, {
+      "method": "fs.download",
+      "params": {
+        "uri": "https://myshell-public-repo-host.s3.amazonaws.com/openvoice/checkpoints_1226.zip",
+        "path": "app/ckpt.zip"
+      }
+    }, {
+      "method": "notify",
+      "params": {
+        "html": "Click the 'start' tab to get started!"
+      }
+    }]
+  }
 }
